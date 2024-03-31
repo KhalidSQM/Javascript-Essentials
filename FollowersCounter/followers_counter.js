c8ae1de25ff0d5c7c18b16773e3f90069b0c00ab
KhalidSQM <KhalidSQM@gmail.com>
2024-03-31 12:42:28 +0200

    Followers Counter

---
+++ FollowersCounter/followers_counter.js
@@ -0,0 +1,19 @@
+let count = 0;
+
+function increaseCount() {
+  count++;
+  displayCount();
+  checkCountValue();
+}
+
+function displayCount() {
+  document.getElementById("countDisplay").innerHTML = count;
+}
+
+function checkCountValue() {
+  if (count === 10) {
+    alert("Your Instagram post gained 10 followers! congratulations");
+  } else if (count === 20) {
+    alert("Your Instagram gained 20 followers! keep it up");
+  }
+}
